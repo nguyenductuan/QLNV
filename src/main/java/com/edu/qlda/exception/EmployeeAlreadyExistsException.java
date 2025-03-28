24c1461d3714959507d8e56162047edd95efd7cf
nguyenductuan <nguyenductuanbk1234@gmail.com>
2025-03-28 22:13:29 +0700

    git push origin

--- src/main/java/com/edu/qlda/exception/EmployeeAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.edu.qlda.exception;
-
-public class EmployeeAlreadyExistsException extends RuntimeException {
-    public EmployeeAlreadyExistsException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/edu/qlda/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.edu.qlda.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
