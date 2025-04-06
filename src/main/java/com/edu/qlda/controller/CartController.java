4abd3261dda5d35a748bdcf8671d85a28a9b1f18
nguyenductuan <nguyenductuanbk1234@gmail.com>
2025-04-06 16:07:17 +0700

    Edit code

--- src/main/java/com/edu/qlda/controller/CartController.java
+++ src/main/java/com/edu/qlda/controller/CartController.java
@@ -15,15 +15,12 @@ import java.util.List;
 @CrossOrigin("http://localhost:4200")
 
 public class CartController {
-
     private final CartService cartService;
     public  CartController(CartService cartService){
         this.cartService=cartService;
     }
-
     private static final String ACTION_SUCCESS = "Thành cong";
     @PostMapping("cart/add-product")
-
     public  ResponseEntity<Messageresponse<Void>> addProductToCart(@RequestBody CartrequestDto request) {
         cartService.addProductToCart(request);
         Messageresponse <Void> response = new Messageresponse(200, ACTION_SUCCESS, "");// Trả về ResponseEntity với mã trạng thái HTTP là OK (200)
@@ -43,10 +40,23 @@ public class CartController {
         cartService.deleteproduct(id);
         return ACTION_SUCCESS;
     }
-    // Xóa nhiều sản phẩm trong giỏ hàng
-    @DeleteMapping
-    public ResponseEntity<String> deleteCartItems(@RequestBody List<Integer> cartItemIds) {
-        cartService.deleteCartItems(cartItemIds);
-        return ResponseEntity.ok("Các sản phẩm đã được xóa khỏi giỏ hàng thành công");
+    // Xóa nhiều sản phẩm trong giỏ hàng cần sửa
+    @DeleteMapping("delete-producttoCart/[ids]")
+    public ResponseEntity<Messageresponse<List<Integer>>> deleteCartItems(@RequestBody List<Integer> ids) {
+        try {
+            // Kiểm tra danh sách có rỗng không
+            if (ids == null || ids.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).body(new Messageresponse<>(404, "Danh sách mã giảm giá cần xóa không được để trống"));
+            }
+            // danh sách mã giảm giá cần xóa
+            List<Integer> notFoundIds = cartService.deleteProducts(ids);
+            if (notFoundIds.isEmpty()) {
+                return ResponseEntity.ok(new Messageresponse<>(200, "Các mã giảm giá đã được xóa thành công"));
+            } else {
+                return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).body(new Messageresponse<>(404, "Một số mã giảm giá không tồn tại", notFoundIds));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Messageresponse<>(500, "Lỗi hệ thống:" + e.getMessage()));
+        }
     }
 }
